2d8f345a498fbfdba2c86f1ab45285acc37a79d6
SolalSTR <sstradella@gmail.com>
2019-10-02 17:30:35 +0200

    nouveau plateau

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 class Plateau {
     constructor(cases,plateau,players) {
         this.plateau = plateau;
-        this.cases = cases;
-        this.casesElement = [];
+        this.plateauCases = cases;
+
         this.playersName = players;
         this.players = [];
         this.createPlateCase();
@@ -10,131 +10,143 @@ class Plateau {
     }
 
     createPlateCase() {
-        let corners = [
-            document.querySelector(".case-start"),
-            document.querySelector(".case-prison"),
-            document.querySelector(".case-parc"),
-            document.querySelector(".case-police")
-        ];
-        for (let i in Object.keys(this.cases)) {
-            let side = Object.keys(this.cases)[i];
-            let ruesContainer = document.querySelector(".case-rue-" + side);
-            let color = this.cases[side].color;
-            let rueElements = [];
-            for (let rue of Object.keys(this.cases[side])) {
-                if (rue == "color") {
-                    continue;
-                }
-                let pionDiv = document.createElement("div");
-                pionDiv.classList.add("pionDiv");
+        this.plateau.style.gridTemplate = "repeat(3,1fr) / repeat(3,1fr)";
+        let cardIndex = 0;
+        let x = 3;
+        let y = 3;
+        for (let cases of Object.keys(this.plateauCases)) {
 
-                let card = document.createElement("div");
-                let cardName = document.createElement("p");
-                cardName.style.background = color
-                cardName.innerText = rue;
+            let card = this.plateauCases[cases];
 
+            let cardElement = document.createElement("div");
+
+            if (card.function == "start") {
+                let iconElemnt = document.createElement("i");
+                iconElemnt.classList = "fas fa-long-arrow-alt-left";
+                cardElement.appendChild(iconElemnt);
+            }
+            if (card.function == "jail") {
+                let iconElemnt = document.createElement("i");
+                iconElemnt.classList = "fas fa-dungeon";
+                cardElement.appendChild(iconElemnt);
+            }
+            if (card.function == "parc") {
+                let iconElemnt = document.createElement("i");
+                iconElemnt.classList = "fas fa-car-alt";
+                cardElement.appendChild(iconElemnt);
+            }
+            if (card.function == "police") {
+                let iconElemnt = document.createElement("i");
+                iconElemnt.classList = "fas fa-angry";
+                cardElement.appendChild(iconElemnt);
+            }
+
+            let cardName = document.createElement("p");
+            cardName.style.background = card.color;
+            cardName.innerText = card.name;
+            cardElement.appendChild(cardName);
+
+            if (card.function == "sell") {
                 let cardPrice = document.createElement("p");
-                cardPrice.innerText = "Prix: " +this.cases[side][rue].prix;
+                cardPrice.innerText = "Prix: " +card.prix;
+                cardElement.appendChild(cardPrice);
 
                 let cardLoyer = document.createElement("p");
-                cardLoyer.innerText = "Loyer: " +this.cases[side][rue].loyer;
-
-                card.appendChild(cardName);
-                card.appendChild(cardPrice);
-                card.appendChild(cardLoyer);
-                card.appendChild(pionDiv);
-                ruesContainer.appendChild(card);
-                rueElements.push(card);
+                cardLoyer.innerText = "Loyer: " + card.loyer;
+                cardElement.appendChild(cardLoyer);
             }
-            this.casesElement = this.casesElement.concat(corners[i],rueElements);
+            let platLength = this.plateauCases.length;
+            cardElement.style.order = cardIndex;
+
+            cardElement.style.gridColumn = x + " / span 1";
+            cardElement.style.gridRow = y + " / span 1";
+            cardIndex++;
+            if (cardIndex >= 7) {
+                y++;
+            } else if (cardIndex >= 5) {
+                x++;
+            } else if (cardIndex >= 3) {
+                y--;
+            } else {
+                x--;
+            }
+
+            cardElement.classList.add("case");
+            this.plateau.appendChild(cardElement);
+            card.element = cardElement;
         }
-        console.log(this.casesElement);
     }
 
     createPlayerPions() {
         for (let player of this.playersName) {
-            this.players.push(new Player(player,"red",50000,this.casesElement));
+            this.players.push(new Player(player,"red",500,this.plateauCases));
         }
     }
 }
 
-var cases = {
-    bottom: {
-        color: "#4ee350",
-        Pompidou: {
-            loyer: 10, prix: 50
-        },
-        Séville: {
-            loyer: 10, prix: 60
-        },
-        Oui: {
-            loyer: 10, prix: 70
-        },
-        Pompidou_2: {
-            loyer: 10, prix: 80
-        },
-        Non: {
-            loyer: 10, prix: 50
-        }
+
+
+
+var plateauCases = {
+    start: {
+        function: "start",
+        name: "Start",
+        prix: 200,
+        element: null
     },
-    left: {
-        color: "#e34e4e",
-        Pompidou: {
-            loyer: 10, prix: 50
-        },
-        Séville: {
-            loyer: 10, prix: 60
-        },
-        Oui: {
-            loyer: 10, prix: 70
-        },
-        Pompidou_2: {
-            loyer: 10, prix: 80
-        },
-        Non: {
-            loyer: 10, prix: 50
-        }
+    pompidou: {
+        function: "sell",
+        name: "Pompidou",
+        prix: 10,
+        color: "hsl(57, 75%, 50%)",
+        loyer: 5,
+        element: null
     },
-    top: {
-        color: "#e3d94e",
-        Pompidou: {
-            loyer: 10, prix: 50
-        },
-        Séville: {
-            loyer: 10, prix: 60
-        },
-        Oui: {
-            loyer: 10, prix: 70
-        },
-        Pompidou_2: {
-            loyer: 10, prix: 80
-        },
-        Non: {
-            loyer: 10, prix: 50
-        }
+    jail: {
+        function: "jail",
+        name: "Jail",
+        waitTime: 3,
+        element: null
     },
-    right: {
-        color: "#4e54e3",
-        Pompidou: {
-            loyer: 10, prix: 50
-        },
-        Séville: {
-            loyer: 10, prix: 60
-        },
-        Oui: {
-            loyer: 10, prix: 70
-        },
-        Pompidou_2: {
-            loyer: 10, prix: 80
-        },
-        Non: {
-            loyer: 10, prix: 50
-        }
+    partDieu: {
+        function: "sell",
+        name: "Part-Dieu",
+        prix: 20,
+        color: "hsl(0, 74%, 50%)",
+        loyer: 15,
+        element: null
+    },
+    parc: {
+        function: "parc",
+        name: "Free Parc",
+        element: null
+    },
+    villeurbanne: {
+        function: "sell",
+        name: "Villeurbanne",
+        prix: 100,
+        color: "hsl(120, 74%, 50%)",
+        loyer: 175,
+        element: null
+    },
+    police: {
+        function: "police",
+        name: "Go to prison",
+        element: null
+    },
+    lafayette: {
+        function: "sell",
+        name: "Lafayette",
+        prix: 500,
+        color: "hsl(246, 74%, 50%)",
+        loyer: 600,
+        element: null
     }
 }
 
-var game = new Plateau(cases,document.querySelector(".plateau"),["Solal","sOLAL"]);
+var game = new Plateau(plateauCases,document.getElementById('plateau'),["Solal","sOLAL","simon"]);
+/*
+setInterval(function() {
 
-setTimeout(function() {
-    game.players[0].movePion(4);
-},1000);
+},1000);*/
+game.players[0].movePion(2);
--- js/player.js
+++ js/player.js
@@ -1,8 +1,17 @@
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
 class Player {
     constructor(name,color,money,plateau) {
         this.name = name;
         this.color = color;
         this.money = money;
+        this.card = [];
         this.plateau = plateau;
         this.pos = 0;
 
@@ -15,25 +24,64 @@ class Player {
         pion.classList.add("pion");
         pion.style.background = this.color;
         this.pion = pion;
+        document.getElementById('plateau').appendChild(this.pion);
         this.setPos();
     }
 
     movePion(step) {
         let index = this.pos + step;
+        let platLength = Object.size(this.plateau);
         if (index < 0) {
-            index += this.plateau.length;
+            index += platLength;
         }
-        if (index >= this.plateau.length) {
-            index -= this.plateau.length;
+        if (index >= platLength) {
+            index -= platLength;
         }
         this.pos = index;
         this.setPos();
+        this.popUp("Buy ?",this.test,"Yes",this.test2,"No");
+    }
+
+    popUp(title,func1,name1,func2,name2) {
+        let popUpElement = document.createElement("div");
+        popUpElement.classList.add("popUp");
+
+        let popUpTitle = document.createElement("p");
+        popUpTitle.innerText = title;
+        popUpElement.appendChild(popUpTitle);
+
+        let buttonDiv = document.createElement("div");
+
+        popUpElement.appendChild(buttonDiv);
+
+        let confirm = document.createElement("button");
+        confirm.innerText = name1;
+        confirm.onclick = function() {
+            popUpElement.remove();
+            func1()
+        }
+        buttonDiv.appendChild(confirm);
+
+        let cancel = document.createElement("button");
+        cancel.innerText = name2;
+        cancel.onclick = function() {
+            popUpElement.remove();
+            func2()
+        }
+        buttonDiv.appendChild(cancel);
+
+        document.getElementById('plateau').appendChild(popUpElement);
 
     }
 
-    setPos = () => {
+    test = () => {console.log('oui');}
+    test2 = () => {console.log('non');}
 
-        this.plateau[this.pos].getElementsByTagName('div')[0].appendChild(this.pion);
+    setPos = () => {
+        let element = this.plateau[Object.keys(this.plateau)[this.pos]].element;
+
+        this.pion.style.top = element.offsetTop + element.scrollHeight/2 - this.pion.scrollHeight/2 + "px";
+        this.pion.style.left = element.offsetLeft + element.scrollWidth/2 - this.pion.scrollWidth/2 + "px";
 
     }
 }
